e5b0376a43dbf5c32f01ba0dd1aab6c06634c0dc
Mikkel Malmberg <mikkelmalmberg@me.com>
2013-11-17 11:14:52 +0100

    capitalization

--- application.rb
+++ application.rb
@@ -8,7 +8,7 @@ class Ordbook < Sinatra::Base
   end
 
   get "/lookup.json" do
-    @words = Word.where("word LIKE ?", "#{params[:q]}%").limit(20).order("word").all
+    @words = Word.where("word LIKE ?", "#{params[:q].downcase}%").limit(20).order("word").all
     content_type 'application/json'
     { words: @words }.to_json
   end

bc280d1ec19b532a5f9afd9fb924f74f6eb62abb
Mikkel Malmberg <mikkel@brnbw.com>
2016-01-27 12:14:45 +0100

    Update deploy script

--- bin/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-DIR="/var/www/apps/2sek.dk"
-UPDATE="git pull"
-BUNDLE="zsh --login -c 'bundle --deployment'"
-RESTART="touch tmp/restart.txt"
-ssh brnbw.com "cd $DIR && $UPDATE && $BUNDLE && $RESTART"
